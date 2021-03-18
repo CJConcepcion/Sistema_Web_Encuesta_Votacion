aec642a1d52b805656c5f64f425482bbd47ab27f
CJConcepcion <68117085+CJConcepcion@users.noreply.github.com>
2021-03-18 07:39:50 -0500

    Create bootbox.js

---
+++ bootstrap/js/bootbox.js
@@ -0,0 +1,8 @@
+Swal.fire({
+  title: 'BIENVENIDO (A)',
+  text: 'Este sitio trata sobre Las Bases de Datos y su AdministraciÃ³n. Esperamos poder ayudarte a reforzar tus conocimientos, mediante el contenido que aquÃ­ estÃ¡ alojado.',
+  imageUrl: 'image/blog2.png',
+  imageWidth: 400,
+  imageHeight: 200,
+  imageAlt: 'Custom image',
+});
